9302eaac8e79812ff220934ab73522e484a883d1
L. K. <tuloukas@gmail.com>
2024-06-05 10:25:43 +0200

    Refactor getting submodel property into simpler getter and setter functions

--- src/live-data-couchdb.py
+++ src/live-data-couchdb.py
@@ -31,40 +31,35 @@ basyx.aas.backend.couchdb.register_credentials(couchdb_url, couchdb_user, couchd
 
 # Now, we create a CouchDBObjectStore as an interface for managing the objects in the CouchDB server.
 couchdb_object_store = basyx.aas.backend.couchdb.CouchDBObjectStore(couchdb_url, couchdb_database)
-
 # For reading auxiliary files, should not be needed in this case
 file_store = aasx.DictSupplementaryFileContainer()
 
-with aasx.AASXReader("../machine1.aasx") as reader:
+with aasx.AASXReader("../machine1_speiser.aasx") as reader:
     # Read all contained AAS objects and all referenced auxiliary files
     reader.read_into(object_store=couchdb_object_store,
                      file_store=file_store)
+    
+# with aasx.AASXReader("../machine2_krempel.aasx") as reader:
+#     # Read all contained AAS objects and all referenced auxiliary files
+#     reader.read_into(object_store=couchdb_object_store,
+#                         file_store=file_store)
+    
+# with aasx.AASXReader("../machine2_leger.aasx") as reader:
+#     # Read all contained AAS objects and all referenced auxiliary files
+#     reader.read_into(object_store=couchdb_object_store,
+#                         file_store=file_store)
 
-machine_state_submodel_reference = model.ModelReference(
-    (model.Key(
-        type_=model.KeyTypes.SUBMODEL,
-        value='https://ita.rwth-aachen.de/machine-state-submodel'),
-    ),
-    type_=model.Submodel
-)
+def set_submodel_property_value(submodel_id, property_name, value):
+    machine_state_submodel = couchdb_object_store.get_identifiable(submodel_id)
+    machine_state_submodel.update()
+    machine_state_submodel.get_referable(property_name).value = value
+    machine_state_submodel.get_referable(property_name).commit()
+    machine_state_submodel.update()
 
-machine_state_submodel = machine_state_submodel_reference.resolve(couchdb_object_store)
-print(machine_state_submodel)
-# print(machine_state_submodel.get_referable("quetschwalze_1_drehzahl_m_min").)
-
-property_reference = model.ModelReference(
-           (model.Key(
-                type_=model.KeyTypes.SUBMODEL,
-                value='https://ita.rwth-aachen.de/machine-state-submodel'),
-            model.Key(
-                type_=model.KeyTypes.PROPERTY,
-                value='quetschwalze_1_drehzahl_m_min'),
-            ),
-            type_=model.Property
-        )
-
-quetschwalze_1_drehzahl_couchdb = property_reference.resolve(couchdb_object_store)
-print(quetschwalze_1_drehzahl_couchdb.value)
+def get_submodel_property_value(submodel_id, property_name):
+    machine_state_submodel = couchdb_object_store.get_identifiable(submodel_id)
+    machine_state_submodel.update()
+    return machine_state_submodel.get_referable(property_name).value
 
 client = Client("opc.tcp://localhost:4840/freeopcua/server/")
 # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
@@ -72,15 +67,14 @@ client.connect()
 
 # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
 root = client.get_root_node()
-print("Objects node is: ", root)
+print("Objects node of OPCUA is: ", root)
 # Node objects have methods to read and write node attributes as well as browse or populate address space
-print("Children of root are: ", root.get_children())
+print("Children of OPCUA root are: ", root.get_children())
 
 while not exit_flag:
-    # Read data from OPC UA, write it to the CouchDB Submodel Object store and commit the changes.
     before = time.time()
-
     try:
+        # Read data from OPC UA, write it to the CouchDB Submodel Object store and commit the changes.
         # TODO: Consider renaming properties in submodel to the same as OPC UA server so that all properties can be iterated over and fetched without manually specifying them all
         # TODO: Put this into separate functions
         # TODO: One class for each machine, standardized to avoid rewriting code, write pseudocode first to plan
@@ -89,8 +83,7 @@ while not exit_flag:
         opc_ua_property_value = root.get_child(["0:Objects", "2:machine1", f"2:{opc_ua_property_name}"]).get_value()
         print(f"{opc_ua_property_name} value is:", opc_ua_property_value)
 
-        quetschwalze_1_drehzahl_couchdb.value = opc_ua_property_value
-        quetschwalze_1_drehzahl_couchdb.commit()
+        set_submodel_property_value("https://ita.rwth-aachen.de/machine-state-submodel", "quetschwalze_1_drehzahl", opc_ua_property_value)
     except _auto.BadNoMatch as e:
         print(f"Property={opc_ua_property_name} was not found on OPC UA server, skipping:", e)
     except Exception as e:
